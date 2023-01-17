e319351f1cbc8374cd814c31973a70e6495a5c9b
André Lopes <andrelneres@hotmail.com>
2023-01-17 15:36:22 -0300

    Relacionamentos e Migrações

--- CasaDoCodigo/ApplicationContext.cs
+++ CasaDoCodigo/ApplicationContext.cs
@@ -14,6 +14,17 @@ namespace CasaDoCodigo
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Produto>().HasKey(x => x.Id);
+
+            modelBuilder.Entity<Pedido>().HasKey(x => x.Id);
+            modelBuilder.Entity<Pedido>().HasMany(x => x.Itens).WithOne(x => x.Pedido);
+            modelBuilder.Entity<Pedido>().HasOne(x => x.Cadastro).WithOne(x => x.Pedido).IsRequired();
+
+            modelBuilder.Entity<ItemPedido>().HasKey(x => x.Id);
+            modelBuilder.Entity<ItemPedido>().HasOne(x => x.Pedido);
+            modelBuilder.Entity<ItemPedido>().HasOne(x => x.Produto);
+
+            modelBuilder.Entity<Cadastro>().HasKey(x => x.Id);
+            modelBuilder.Entity<Cadastro>().HasOne(x => x.Pedido);
         }
     }
 }
