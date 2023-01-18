19f8ff84487f5d35b3e9c9effac99a0ba01bf759
André Lopes <andrelneres@hotmail.com>
2023-01-18 15:26:03 -0300

    Configurando session, gravando e lendo PedidoId na sessão

--- CasaDoCodigo/Repositories/PedidoRepository.cs
+++ CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -1,4 +1,5 @@
 ﻿using CasaDoCodigo.Models;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,8 +12,20 @@ namespace CasaDoCodigo.Repositories
     }
     public class PedidoRepository : BaseRepository<Pedido>, IPedidoRepository
     {
-        public PedidoRepository(ApplicationContext contexto) : base(contexto)
+        private readonly IHttpContextAccessor _contextAccessor;
+        public PedidoRepository(ApplicationContext contexto, IHttpContextAccessor contextAccessor) : base(contexto)
         {
+            _contextAccessor = contextAccessor;
+        }
+
+        private int? GetPedidoId()
+        {
+            return _contextAccessor.HttpContext.Session.GetInt32("pedidoId");
+        }
+
+        private void SetPedidoId(int pedidoId)
+        {
+            _contextAccessor.HttpContext.Session.SetInt32("pedidoId", pedidoId);
         }
     }
 }
