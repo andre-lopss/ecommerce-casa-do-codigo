5401999702f631af4a08dba32c6c75454ae3c6ad
André Lopes <andrelneres@hotmail.com>
2023-01-18 09:37:09 -0300

    Lendo Arquivo Json de Produtos

--- CasaDoCodigo/DataService.cs
+++ CasaDoCodigo/DataService.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,7 +20,16 @@ namespace CasaDoCodigo
         public void InicializaDB()
         {
             _contexto.Database.EnsureCreated();
-        }
+            var json = File.ReadAllText("livros.json");
 
+            var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+        }      
+    }
+
+    class Livro
+    {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
     }
 }
