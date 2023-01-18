fb0e9966b09606a12ce7c9179eabd7b769379a3a
André Lopes <andrelneres@hotmail.com>
2023-01-18 14:56:31 -0300

    Links com Tag Helpers

--- CasaDoCodigo/Repositories/ItemPedidoRepository.cs
+++ CasaDoCodigo/Repositories/ItemPedidoRepository.cs
@@ -9,7 +9,7 @@ namespace CasaDoCodigo.Repositories
     public interface IItemPedidoRepository
     {
     }
-    public class ItemPedidoRepository : BaseRepository<ItemPedido>, ItemPedidoRepository
+    public class ItemPedidoRepository : BaseRepository<ItemPedido>, IItemPedidoRepository
     {
         public ItemPedidoRepository(ApplicationContext contexto) : base(contexto)
         {
