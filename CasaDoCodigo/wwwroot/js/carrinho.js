0858841c0edad546e05832499e0ed60e1e2d1d5d
André Lopes <andrelneres@hotmail.com>
2023-01-20 10:42:44 -0300

    Update carrinho.js

--- CasaDoCodigo/wwwroot/js/carrinho.js
+++ CasaDoCodigo/wwwroot/js/carrinho.js
@@ -1,12 +1,12 @@
 ﻿class Carrinho {
-    clickIncremento(btn) {
-        let data = this.getData(btn);
+    clickIncremento(button) {
+        let data = this.getData(button);
         data.Quantidade++;
         this.postQuantidade(data);
     }
 
-    clickDecremento(btn) {
-        let data = this.getData(btn);
+    clickDecremento(button) {
+        let data = this.getData(button);
         data.Quantidade--;
         this.postQuantidade(data);
     }
@@ -21,16 +21,19 @@
         var itemId = $(linhaDoItem).attr('item-id');
         var novaQuantidade = $(linhaDoItem).find('input').val();
 
-        return{
+        return {
             Id: itemId,
             Quantidade: novaQuantidade
-        }
+        };
     }
 
     postQuantidade(data) {
+
         let token = $('[name=__RequestVerificationToken]').val();
+
         let headers = {};
-        headers['RequestVerificationToken'] = token
+        headers['RequestVerificationToken'] = token;
+
         $.ajax({
             url: '/pedido/updatequantidade',
             type: 'POST',
@@ -39,22 +42,25 @@
             headers: headers
         }).done(function (response) {
             let itemPedido = response.itemPedido;
-            let linhaDoItem = $('[itemB-id=' + itemPedido.id  + ']');
+            let linhaDoItem = $('[item-id=' + itemPedido.id + ']')
             linhaDoItem.find('input').val(itemPedido.quantidade);
             linhaDoItem.find('[subtotal]').html((itemPedido.subtotal).duasCasas());
             let carrinhoViewModel = response.carrinhoViewModel;
             $('[numero-itens]').html('Total: ' + carrinhoViewModel.itens.length + ' itens');
-            $('[total]').html((carrinhoViewModel.total).duasCasas())
+            $('[total]').html((carrinhoViewModel.total).duasCasas());
 
             if (itemPedido.quantidade == 0) {
                 linhaDoItem.remove();
             }
         });
-
     }
 }
+
 var carrinho = new Carrinho();
 
 Number.prototype.duasCasas = function () {
     return this.toFixed(2).replace('.', ',');
 }
+
+
+
